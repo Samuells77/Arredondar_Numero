080cf2ef52a93126ea7dce4d9228451cdc8840a3
Samuel <38147282+Samuells77@users.noreply.github.com>
2023-05-07 15:58:59 -0300

    Add files via upload

---
+++ Arredondar Número.py
@@ -0,0 +1,8 @@
+import math
+n = (float(input("Digite um número: ")))
+arredonda = math.trunc(n)
+print("Arredondando o número {}, o resultado é: {}".format(n, arredonda))
+
+num = (float(input("Digite um número: ")))
+arredonda2 = int(num)
+print("Arredondando o número {}, o resultado é: {}".format(num, arredonda2))
